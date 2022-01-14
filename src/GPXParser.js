fc08882b397b28fe83471de3b7a300907af1a058
Ringo Rohe <hallo@ringorohe.de>
2022-01-14 17:59:09 +0100

    fixing missing variable declarations

--- src/GPXParser.js
+++ src/GPXParser.js
@@ -5,10 +5,10 @@
  */
 let gpxParser = function () {
     this.xmlSource = "";
-    this.metadata  = {};
+    this.metadata = {};
     this.waypoints = [];
-    this.tracks    = [];
-    this.routes    = [];
+    this.tracks = [];
+    this.routes = [];
 };
 
 /**
@@ -25,25 +25,25 @@ gpxParser.prototype.parse = function (gpxstring) {
     this.xmlSource = domParser.parseFromString(gpxstring, 'text/xml');
 
     let metadata = this.xmlSource.querySelector('metadata');
-    if(metadata != null){
-        this.metadata.name  = this.getElementValue(metadata, "name");
-        this.metadata.desc  = this.getElementValue(metadata, "desc");
-        this.metadata.time  = this.getElementValue(metadata, "time");
+    if (metadata != null) {
+        this.metadata.name = this.getElementValue(metadata, "name");
+        this.metadata.desc = this.getElementValue(metadata, "desc");
+        this.metadata.time = this.getElementValue(metadata, "time");
 
         let author = {};
         let authorElem = metadata.querySelector('author');
-        if(authorElem != null){
+        if (authorElem != null) {
             author.name = this.getElementValue(authorElem, "name");
-            author.email  = {};
+            author.email = {};
             let emailElem = authorElem.querySelector('email');
-            if(emailElem != null){
-                author.email.id     = emailElem.getAttribute("id");
+            if (emailElem != null) {
+                author.email.id = emailElem.getAttribute("id");
                 author.email.domain = emailElem.getAttribute("domain");
             }
 
-            let link     = {};
+            let link = {};
             let linkElem = authorElem.querySelector('link');
-            if(linkElem != null){
+            if (linkElem != null) {
                 link.href = linkElem.getAttribute('href');
                 link.text = this.getElementValue(linkElem, "text");
                 link.type = this.getElementValue(linkElem, "type");
@@ -54,7 +54,7 @@ gpxParser.prototype.parse = function (gpxstring) {
 
         let link = {};
         let linkElem = this.queryDirectSelector(metadata, 'link');
-        if(linkElem != null){
+        if (linkElem != null) {
             link.href = linkElem.getAttribute('href');
             link.text = this.getElementValue(linkElem, "text");
             link.type = this.getElementValue(linkElem, "type");
@@ -63,18 +63,18 @@ gpxParser.prototype.parse = function (gpxstring) {
     }
 
     var wpts = [].slice.call(this.xmlSource.querySelectorAll('wpt'));
-    for (let idx in wpts){
+    for (let idx in wpts) {
         var wpt = wpts[idx];
-        let pt  = {};
+        let pt = {};
         pt.name = keepThis.getElementValue(wpt, "name");
-        pt.sym  = keepThis.getElementValue(wpt, "sym");
-        pt.lat  = parseFloat(wpt.getAttribute("lat"));
-        pt.lon  = parseFloat(wpt.getAttribute("lon"));
+        pt.sym = keepThis.getElementValue(wpt, "sym");
+        pt.lat = parseFloat(wpt.getAttribute("lat"));
+        pt.lon = parseFloat(wpt.getAttribute("lon"));
 
-        let floatValue = parseFloat(keepThis.getElementValue(wpt, "ele")); 
+        let floatValue = parseFloat(keepThis.getElementValue(wpt, "ele"));
         pt.ele = isNaN(floatValue) ? null : floatValue;
 
-        pt.cmt  = keepThis.getElementValue(wpt, "cmt");
+        pt.cmt = keepThis.getElementValue(wpt, "cmt");
         pt.desc = keepThis.getElementValue(wpt, "desc");
 
         let time = keepThis.getElementValue(wpt, "time");
@@ -84,21 +84,21 @@ gpxParser.prototype.parse = function (gpxstring) {
     }
 
     var rtes = [].slice.call(this.xmlSource.querySelectorAll('rte'));
-    for (let idx in rtes){
+    for (let idx in rtes) {
         let rte = rtes[idx];
         let route = {};
-        route.name   = keepThis.getElementValue(rte, "name");
-        route.cmt    = keepThis.getElementValue(rte, "cmt");
-        route.desc   = keepThis.getElementValue(rte, "desc");
-        route.src    = keepThis.getElementValue(rte, "src");
+        route.name = keepThis.getElementValue(rte, "name");
+        route.cmt = keepThis.getElementValue(rte, "cmt");
+        route.desc = keepThis.getElementValue(rte, "desc");
+        route.src = keepThis.getElementValue(rte, "src");
         route.number = keepThis.getElementValue(rte, "number");
 
-        let type     = keepThis.queryDirectSelector(rte, "type");
-        route.type   = type != null ? type.innerHTML : null;
+        let type = keepThis.queryDirectSelector(rte, "type");
+        route.type = type != null ? type.innerHTML : null;
 
-        let link     = {};
+        let link = {};
         let linkElem = rte.querySelector('link');
-        if(linkElem != null){
+        if (linkElem != null) {
             link.href = linkElem.getAttribute('href');
             link.text = keepThis.getElementValue(linkElem, "text");
             link.type = keepThis.getElementValue(linkElem, "type");
@@ -108,13 +108,13 @@ gpxParser.prototype.parse = function (gpxstring) {
         let routepoints = [];
         var rtepts = [].slice.call(rte.querySelectorAll('rtept'));
 
-        for (let idxIn in rtepts){
+        for (let idxIn in rtepts) {
             let rtept = rtepts[idxIn];
-            let pt    = {};
-            pt.lat    = parseFloat(rtept.getAttribute("lat"));
-            pt.lon    = parseFloat(rtept.getAttribute("lon"));
+            let pt = {};
+            pt.lat = parseFloat(rtept.getAttribute("lat"));
+            pt.lon = parseFloat(rtept.getAttribute("lon"));
 
-            let floatValue = parseFloat(keepThis.getElementValue(rtept, "ele")); 
+            let floatValue = parseFloat(keepThis.getElementValue(rtept, "ele"));
             pt.ele = isNaN(floatValue) ? null : floatValue;
 
             let time = keepThis.getElementValue(rtept, "time");
@@ -123,31 +123,31 @@ gpxParser.prototype.parse = function (gpxstring) {
             routepoints.push(pt);
         }
 
-        route.distance  = keepThis.calculDistance(routepoints);
+        route.distance = keepThis.calculDistance(routepoints);
         route.elevation = keepThis.calcElevation(routepoints);
-        route.slopes    = keepThis.calculSlope(routepoints, route.distance.cumul);
-        route.points    = routepoints;
+        route.slopes = keepThis.calculSlope(routepoints, route.distance.cumul);
+        route.points = routepoints;
 
         keepThis.routes.push(route);
     }
 
     var trks = [].slice.call(this.xmlSource.querySelectorAll('trk'));
-    for (let idx in trks){
+    for (let idx in trks) {
         let trk = trks[idx];
         let track = {};
 
-        track.name   = keepThis.getElementValue(trk, "name");
-        track.cmt    = keepThis.getElementValue(trk, "cmt");
-        track.desc   = keepThis.getElementValue(trk, "desc");
-        track.src    = keepThis.getElementValue(trk, "src");
+        track.name = keepThis.getElementValue(trk, "name");
+        track.cmt = keepThis.getElementValue(trk, "cmt");
+        track.desc = keepThis.getElementValue(trk, "desc");
+        track.src = keepThis.getElementValue(trk, "src");
         track.number = keepThis.getElementValue(trk, "number");
 
-        let type     = keepThis.queryDirectSelector(trk, "type");
-        track.type   = type != null ? type.innerHTML : null;
+        let type = keepThis.queryDirectSelector(trk, "type");
+        track.type = type != null ? type.innerHTML : null;
 
-        let link     = {};
+        let link = {};
         let linkElem = trk.querySelector('link');
-        if(linkElem != null){
+        if (linkElem != null) {
             link.href = linkElem.getAttribute('href');
             link.text = keepThis.getElementValue(linkElem, "text");
             link.type = keepThis.getElementValue(linkElem, "type");
@@ -156,13 +156,13 @@ gpxParser.prototype.parse = function (gpxstring) {
 
         let trackpoints = [];
         let trkpts = [].slice.call(trk.querySelectorAll('trkpt'));
-	    for (let idxIn in trkpts){
+        for (let idxIn in trkpts) {
             var trkpt = trkpts[idxIn];
             let pt = {};
             pt.lat = parseFloat(trkpt.getAttribute("lat"));
             pt.lon = parseFloat(trkpt.getAttribute("lon"));
 
-            let floatValue = parseFloat(keepThis.getElementValue(trkpt, "ele")); 
+            let floatValue = parseFloat(keepThis.getElementValue(trkpt, "ele"));
             pt.ele = isNaN(floatValue) ? null : floatValue;
 
             let time = keepThis.getElementValue(trkpt, "time");
@@ -170,10 +170,10 @@ gpxParser.prototype.parse = function (gpxstring) {
 
             trackpoints.push(pt);
         }
-        track.distance  = keepThis.calculDistance(trackpoints);
+        track.distance = keepThis.calculDistance(trackpoints);
         track.elevation = keepThis.calcElevation(trackpoints);
-        track.slopes    = keepThis.calculSlope(trackpoints, track.distance.cumul);
-        track.points    = trackpoints;
+        track.slopes = keepThis.calculSlope(trackpoints, track.distance.cumul);
+        track.points = trackpoints;
 
         keepThis.tracks.push(track);
     }
@@ -187,9 +187,9 @@ gpxParser.prototype.parse = function (gpxstring) {
  * 
  * @return {} The element value
  */
-gpxParser.prototype.getElementValue = function(parent, needle){
+gpxParser.prototype.getElementValue = function (parent, needle) {
     let elem = parent.querySelector(needle);
-    if(elem != null){
+    if (elem != null) {
         return elem.innerHTML != undefined ? elem.innerHTML : elem.childNodes[0].data;
     }
     return elem;
@@ -204,17 +204,17 @@ gpxParser.prototype.getElementValue = function(parent, needle){
  * 
  * @return {} The element value
  */
-gpxParser.prototype.queryDirectSelector = function(parent, needle) {
+gpxParser.prototype.queryDirectSelector = function (parent, needle) {
 
-    let elements  = parent.querySelectorAll(needle);
+    let elements = parent.querySelectorAll(needle);
     let finalElem = elements[0];
 
-    if(elements.length > 1) {
+    if (elements.length > 1) {
         let directChilds = parent.childNodes;
 
-        for(idx in directChilds) {
+        for (let idx in directChilds) {
             elem = directChilds[idx];
-            if(elem.tagName === needle) {
+            if (elem.tagName === needle) {
                 finalElem = elem;
             }
         }
@@ -230,12 +230,12 @@ gpxParser.prototype.queryDirectSelector = function(parent, needle) {
  * 
  * @return {DistanceObject} An object with total distance and Cumulative distances
  */
-gpxParser.prototype.calculDistance = function(points) {
+gpxParser.prototype.calculDistance = function (points) {
     let distance = {};
     let totalDistance = 0;
     let cumulDistance = [];
     for (var i = 0; i < points.length - 1; i++) {
-        totalDistance += this.calcDistanceBetween(points[i],points[i+1]);
+        totalDistance += this.calcDistanceBetween(points[i], points[i + 1]);
         cumulDistance[i] = totalDistance;
     }
     cumulDistance[points.length - 1] = totalDistance;
@@ -262,13 +262,13 @@ gpxParser.prototype.calcDistanceBetween = function (wpt1, wpt2) {
     latlng2.lat = wpt2.lat;
     latlng2.lon = wpt2.lon;
     var rad = Math.PI / 180,
-		    lat1 = latlng1.lat * rad,
-		    lat2 = latlng2.lat * rad,
-		    sinDLat = Math.sin((latlng2.lat - latlng1.lat) * rad / 2),
-		    sinDLon = Math.sin((latlng2.lon - latlng1.lon) * rad / 2),
-		    a = sinDLat * sinDLat + Math.cos(lat1) * Math.cos(lat2) * sinDLon * sinDLon,
-		    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	return 6371000 * c;
+        lat1 = latlng1.lat * rad,
+        lat2 = latlng2.lat * rad,
+        sinDLat = Math.sin((latlng2.lat - latlng1.lat) * rad / 2),
+        sinDLon = Math.sin((latlng2.lon - latlng1.lon) * rad / 2),
+        a = sinDLat * sinDLat + Math.cos(lat1) * Math.cos(lat2) * sinDLon * sinDLon,
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return 6371000 * c;
 };
 
 /**
@@ -285,9 +285,9 @@ gpxParser.prototype.calcElevation = function (points) {
 
     for (var i = 0; i < points.length - 1; i++) {
         let rawNextElevation = points[i + 1].ele;
-        let rawElevation =  points[i].ele;
+        let rawElevation = points[i].ele;
 
-        if(rawNextElevation !== null && rawElevation !== null) {
+        if (rawNextElevation !== null && rawElevation !== null) {
             let diff = parseFloat(rawNextElevation) - parseFloat(rawElevation);
 
             if (diff < 0) {
@@ -304,7 +304,7 @@ gpxParser.prototype.calcElevation = function (points) {
     for (var i = 0, len = points.length; i < len; i++) {
         let rawElevation = points[i].ele;
 
-        if(rawElevation !== null) {
+        if (rawElevation !== null) {
             var ele = parseFloat(points[i].ele);
             elevation.push(ele);
             sum += ele;
@@ -328,14 +328,14 @@ gpxParser.prototype.calcElevation = function (points) {
  * 
  * @returns {SlopeObject} An array of slopes
  */
-gpxParser.prototype.calculSlope = function(points, cumul) {
+gpxParser.prototype.calculSlope = function (points, cumul) {
     let slopes = [];
 
     for (var i = 0; i < points.length - 1; i++) {
         let point = points[i];
-        let nextPoint = points[i+1];
+        let nextPoint = points[i + 1];
         let elevationDiff = nextPoint.ele - point.ele;
-        let distance = cumul[i+1] - cumul[i];
+        let distance = cumul[i + 1] - cumul[i];
 
         let slope = (elevationDiff * 100) / distance;
         slopes.push(slope);
@@ -362,7 +362,7 @@ gpxParser.prototype.toGeoJSON = function () {
         },
     };
 
-    for(idx in this.tracks) {
+    for (let idx in this.tracks) {
         let track = this.tracks[idx];
 
         var feature = {
@@ -375,17 +375,17 @@ gpxParser.prototype.toGeoJSON = function () {
             }
         };
 
-        feature.properties.name   = track.name;
-        feature.properties.cmt    = track.cmt;
-        feature.properties.desc   = track.desc;
-        feature.properties.src    = track.src;
+        feature.properties.name = track.name;
+        feature.properties.cmt = track.cmt;
+        feature.properties.desc = track.desc;
+        feature.properties.src = track.src;
         feature.properties.number = track.number;
-        feature.properties.link   = track.link;
-        feature.properties.type   = track.type;
+        feature.properties.link = track.link;
+        feature.properties.type = track.type;
 
-        for(idx in track.points) {
+        for (let idx in track.points) {
             let pt = track.points[idx];
-        
+
             var geoPt = [];
             geoPt.push(pt.lon);
             geoPt.push(pt.lat);
@@ -397,7 +397,7 @@ gpxParser.prototype.toGeoJSON = function () {
         GeoJSON.features.push(feature);
     }
 
-    for(idx in this.routes) {
+    for (let idx in this.routes) {
         let track = this.routes[idx];
 
         var feature = {
@@ -410,18 +410,18 @@ gpxParser.prototype.toGeoJSON = function () {
             }
         };
 
-        feature.properties.name   = track.name;
-        feature.properties.cmt    = track.cmt;
-        feature.properties.desc   = track.desc;
-        feature.properties.src    = track.src;
+        feature.properties.name = track.name;
+        feature.properties.cmt = track.cmt;
+        feature.properties.desc = track.desc;
+        feature.properties.src = track.src;
         feature.properties.number = track.number;
-        feature.properties.link   = track.link;
-        feature.properties.type   = track.type;
+        feature.properties.link = track.link;
+        feature.properties.type = track.type;
 
 
-        for(idx in track.points) {
+        for (let idx in track.points) {
             let pt = track.points[idx];
-        
+
             var geoPt = [];
             geoPt.push(pt.lon);
             geoPt.push(pt.lat);
@@ -433,9 +433,9 @@ gpxParser.prototype.toGeoJSON = function () {
         GeoJSON.features.push(feature);
     }
 
-    for(idx in this.waypoints) {
+    for (let idx in this.waypoints) {
         let pt = this.waypoints[idx];
-    
+
         var feature = {
             "type": "Feature",
             "geometry": {
@@ -448,7 +448,7 @@ gpxParser.prototype.toGeoJSON = function () {
 
         feature.properties.name = pt.name;
         feature.properties.sym = pt.sym;
-        feature.properties.cmt  = pt.cmt;
+        feature.properties.cmt = pt.cmt;
         feature.properties.desc = pt.desc;
 
         feature.geometry.coordinates = [pt.lon, pt.lat, pt.ele];
@@ -459,7 +459,7 @@ gpxParser.prototype.toGeoJSON = function () {
     return GeoJSON;
 };
 
-if(typeof module !== 'undefined'){
+if (typeof module !== 'undefined') {
     require('jsdom-global')();
     module.exports = gpxParser;
 }
